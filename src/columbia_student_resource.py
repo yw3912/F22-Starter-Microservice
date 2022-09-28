d7ffddafd7b8496f0a26f1ed490381ae9051df1d
yw3912 <yw3912@columbia.edu>
2022-09-27 20:00:16 -0400

    fix bugs

--- src/columbia_student_resource.py
+++ src/columbia_student_resource.py
@@ -11,27 +11,36 @@ class ColumbiaStudentResource:
     @staticmethod
     def _get_connection():
 
-        usr = os.environ.get("DBUSER")
-        pw = os.environ.get("DBPW")
-        h = os.environ.get("DBHOST")
+        usr = os.environ.get("root")
+        pw = os.environ.get("dbuserdbuser")
+        h = os.environ.get("localhost")
+
+        # conn = pymysql.connect(
+        #     user=usr,
+        #     password=pw,
+        #     host=h,
+        #     cursorclass=pymysql.cursors.DictCursor,
+        #     autocommit=True
+        # )
 
         conn = pymysql.connect(
-            user=usr,
-            password=pw,
-            host=h,
+            host="localhost",
+            port=3306,
+            user="root",
+            password="dbuserdbuser",
             cursorclass=pymysql.cursors.DictCursor,
-            autocommit=True
-        )
+            autocommit=True)
+
         return conn
 
     @staticmethod
     def get_by_key(key):
 
-        sql = "SELECT * FROM f22_databases.columbia_students where guid=%s";
+        # sql = "SELECT * FROM f22_databases.columbia_students where guid=%s"
+        sql = "SELECT * FROM f22_databases.columbia_student where uni=%s"
         conn = ColumbiaStudentResource._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=key)
         result = cur.fetchone()
 
         return result
-
